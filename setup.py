79590526517e38db8f31dce0739fed6fd03c3dfc
Kirill Perevozchikov <kperevozchikov@cybervisiontech.com>
2014-03-18 14:25:30 +0200

    add requirements

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
+import os
+
 from setuptools import setup, find_packages
 import storages
 
-setup(
-    name = 'django-storages',
-    version = storages.__version__,
-    packages = find_packages(),
+SRC_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-    author = 'David Larlet',
-    author_email = 'david@larlet.fr',
-    license = 'BSD',
-    description = 'Support for many storages (S3, MogileFS, etc) in Django.',
+with open(os.path.join(SRC_ROOT, 'requirements.txt')) as f:
+    required = f.read().splitlines()
+
+setup(
+    name='django-storages',
+    version=storages.__version__,
+    packages=find_packages(),
+
+    author='David Larlet',
+    author_email='david@larlet.fr',
+    license='BSD',
+    description='Support for many storages (S3, MogileFS, etc) in Django.',
     url='http://code.welldev.org/django-storages/',
-    classifiers = [
+    install_requires=required,
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
@@ -21,5 +29,5 @@ setup(
         'Framework :: Django',
     ],
     test_suite='tests.main',
-    zip_safe = False,
+    zip_safe=False,
 )
