decebf51de2904f2bc11c5aefd27fdb6b97ff010
Kirill Perevozchikov <kperevozchikov@cybervisiontech.com>
2014-03-18 14:40:51 +0200

    dependies

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ SRC_ROOT = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(SRC_ROOT, 'requirements.txt')) as f:
     required = f.read().splitlines()
 
+dependies = [required.pop() for item in tuple(required) if item.startswith('http')]
+
 setup(
     name='django-storages',
     version=storages.__version__,
@@ -19,6 +21,7 @@ setup(
     description='Support for many storages (S3, MogileFS, etc) in Django.',
     url='http://code.welldev.org/django-storages/',
     install_requires=required,
+    dependency_links=dependies,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
